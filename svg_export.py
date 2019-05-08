ab0b3a4ae08527be4ce9081f062bedf6fa962726
zxyle <zxyful@gmail.com>
2019-05-08 17:32:05 +0800

    完善框架虚线生成

--- svg_export.py
+++ svg_export.py
@@ -18,25 +18,36 @@ def init_svg(params):
     """
 
     half_nose_width = params.get("half_nose_width")
+    half_overall_length = params.get("half_overall_length")
     nose_width = params.get("nose_width")
     nose_length = params.get("nose_length")
     running_length = params.get("running_length")
     overall_length = params.get("overall_length")
     tail_width = params.get("tail_width")
 
-    #
     # write_code("""<line x1="180" y1="0" x2="1380" y2="0" style="stroke:#000000;stroke-width:1"/>""")
-    write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
-            style="stroke:#000000;stroke-width:1"/>""".format(0, half_nose_width, overall_length, half_nose_width))
-    write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
-                style="stroke:#000000;stroke-width:1"/>""".format(nose_length, 0, nose_length, nose_width))
-    write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
-                   style="stroke:#000000;stroke-width:1"/>""".format(nose_length + running_length, 0,
-                                                                     nose_length + running_length, tail_width))
 
+    # 水平中线虚线
     write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
-                       style="stroke:#000000;stroke-width:1" stroke-dasharray="5,5"/>""".format(overall_length / 2, 0,
-                                                                                                overall_length / 2,
+            style="stroke:#000000;stroke-width:1"  stroke-dasharray="5,5"/>""".format(0, half_nose_width,
+                                                                                      overall_length, half_nose_width))
+
+    # 板头虚线
+    write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
+                style="stroke:#000000;stroke-width:1" stroke-dasharray="5,5"/>""".format(nose_length, 0, nose_length,
+                                                                                         nose_width))
+
+    # 板尾虚线
+    write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
+                   style="stroke:#000000;stroke-width:1" stroke-dasharray="5,5"/>""".format(
+        nose_length + running_length, 0,
+        nose_length + running_length, tail_width))
+
+    # TODO 超过部分裁剪掉
+    # 竖直中线虚线
+    write_code("""<line x1="{}" y1="{}" x2="{}" y2="{}"
+                       style="stroke:#000000;stroke-width:1" stroke-dasharray="5,5"/>""".format(half_overall_length, 0,
+                                                                                                half_overall_length,
                                                                                                 nose_width))
 
 

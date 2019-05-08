8596c9724bd980353aae4ede7dc45c0997399710
zxyle <zxyful@gmail.com>
2019-05-08 22:25:50 +0800

    完善各个功能块

--- arc.py
+++ arc.py
@@ -4,7 +4,8 @@
 # Date: 2019-05-07
 # Desc: 有效边刃生成
 
-from math_tools import cal_waist_width
+import numpy as np
+from math import sqrt
 
 
 def gen_arc(params):
@@ -15,19 +16,40 @@ def gen_arc(params):
     """
     half_overall_length = params.get("half_overall_length")
     sidecut_radius = params.get("sidecut_radius")
-    running_length = params.get("running_length")
-    nose_width = params.get("nose_width")
     content = ""
-    # 上圆弧
-    content += ("""<circle cx="{}" cy="{}" r="{}" stroke="black"
-    stroke-width="1" fill="none"/>""".format(half_overall_length, -cal_waist_width(running_length, sidecut_radius),
-                                             sidecut_radius))
 
-    # 下圆弧
-    content += ("""<circle cx="{}" cy="{}" r="{}" stroke="black"
-        stroke-width="1" fill="none"/>""".format(half_overall_length,
-                                                 cal_waist_width(running_length, sidecut_radius) + nose_width,
-                                                 sidecut_radius))
+    # 以下为圆弧代码
+    # # 上圆弧
+    # content += ("""<circle cx="{}" cy="{}" r="{}" stroke="black"
+    # stroke-width="1" fill="none"/>""".format(half_overall_length, -cal_waist_width(running_length, sidecut_radius),
+    #                                          sidecut_radius))
+    #
+    # # 下圆弧
+    # content += ("""<circle cx="{}" cy="{}" r="{}" stroke="black"
+    #     stroke-width="1" fill="none"/>""".format(half_overall_length,
+    #                                              cal_waist_width(running_length, sidecut_radius) + nose_width,
+    #                                              sidecut_radius))
+
+    step = 1
+    code = ""
+    code2 = ""
+    offset = 0
+    for i, x in enumerate(np.arange(180, 1340.00 + step, step), start=1):
+        y = sqrt(pow(sidecut_radius, 2) - pow(half_overall_length - x, 2))
+        y = 10000 - y
+        if i == 1:
+            offset = 300 - y
+            y = offset + y
+            code += "M{} {} ".format(x, y)
+            code2 += "M{} {} ".format(x, 300 - y)
+        else:
+            y += offset
+            code += "L{} {} ".format(x, y)
+            code2 += "L{} {} ".format(x, 300 - y)
+
+    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code))
+
+    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code2))
 
     return content
 
--- boardcam.py
+++ boardcam.py
@@ -4,21 +4,18 @@
 # Date: 2019-05-07
 # Desc: bezier curve -> SVG
 
-from math import sqrt
-
-import numpy as np
-
+from arc import gen_arc
 from inserts import gen_inserts
 from math_tools import gen_bezier
 from svg_export import write_code, init_svg, start_tag, close_tag
 
 if __name__ == "__main__":
-    # P0和P3是endpoints, P1和P2是control points
-    # points = ((0, 0), (40, 140), (90, 280), (180, 150))
     nose_width = 300
+    half_nose_width = nose_width / 2
     nose_length = 180
     overall_length = 1520
     tail_width = 300
+    half_tail_width = tail_width / 2
     tail_length = 180
     running_length = overall_length - tail_length - nose_length
     sidecut_radius = 10000
@@ -27,23 +24,21 @@ if __name__ == "__main__":
     inserts_number = 4
     half_overall_length = overall_length / 2
 
-    left_points = ((0, 150), (50, 140), (90, 180), (180, 0))
+    left_points = ((0, half_nose_width), (50, 140), (90, 180), (nose_length, 0))
+    right_points = ((overall_length, half_tail_width), (4590, 10), (4080, 70), (overall_length - tail_length, 0))
     params = {
         "overall_length": overall_length,
         "half_overall_length": overall_length / 2,
         "running_length": running_length,
         "nose_width": nose_width,
-        "half_nose_width": nose_width / 2,
-        "half_tail_width": tail_width / 2,
+        "half_nose_width": half_nose_width,
+        "half_tail_width": half_tail_width,
         "tail_width": tail_width,
         "nose_length": nose_length,
         "tail_length": tail_length,
         "sidecut_radius": sidecut_radius,
     }
 
-    # 1390,10 1480,10
-    # M1340,0 C1390,10 1480,10 1520,150
-    right_points = ((1520, 150), (5090, 30), (4080, 60), (1340, 0))
     start_tag()
 
     # 板头&板尾生成
@@ -52,39 +47,16 @@ if __name__ == "__main__":
     right_bezier_svg = gen_bezier(right_points)
     write_code(right_bezier_svg)
 
-    #
+    # 初始化SVG文件
     init_svg(params)
 
     # 有效边刃生成
-    # arc_svg = gen_arc(params)
-    # write_code(arc_svg)
-    step = 1
-    content = ""
-    code = ""
-    code2 = ""
-    offset = 0
-    for i, x in enumerate(np.arange(180, 1340.00 + step, step), start=1):
-        y = sqrt(pow(sidecut_radius, 2) - pow(half_overall_length - x, 2))
-        y = 10000 - y
-        if i == 1:
-            offset = 300 - y
-            y = offset + y
-            print(y)
-            code += "M{} {} ".format(x, y)
-            code2 += "M{} {} ".format(x, 300 - y)
-        else:
-            y += offset
-            code += "L{} {} ".format(x, y)
-            code2 += "L{} {} ".format(x, 300 - y)
-
-    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code))
-    write_code(content)
-
-    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code2))
-    write_code(content)
+    arc_svg = gen_arc(params)
+    write_code(arc_svg)
 
     # 嵌件生成
     inserts_svg = gen_inserts(params, 4, 50, 60)
     write_code(inserts_svg)
 
+    # 结束SVG文件
     close_tag()

cfd07059ea02e3db09528c24dabd11da231b5302
zxyle <zxyful@gmail.com>
2019-05-08 15:49:27 +0800

    delete some files

--- demo.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Author: Zheng <zhengxiang@boardcam.org>
-# Date: 2019-03-06
-# Desc: 
--- tests/svg2pdf.py
+++
@@ -1,5 +0,0 @@
-from reportlab.graphics import renderPDF
-from svglib.svglib import svg2rlg
-
-drawing = svg2rlg("output1.svg")
-renderPDF.drawToFile(drawing, "output1.pdf")

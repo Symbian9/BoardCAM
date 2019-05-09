49ed7b90cb4d87ae6068af0a1b140af258fd67e8
zxyle <zxyful@gmail.com>
2019-05-09 14:02:57 +0800

    修复板头&板尾长度变化 引起图形破裂

--- arc.py
+++ arc.py
@@ -11,49 +11,35 @@ import numpy as np
 
 def gen_arc(params):
     """
-    有效边刃生成
+    有效边刃路径生成
     :param params: 参数
     :return:
     """
     half_overall_length = params.get("half_overall_length")
     sidecut_radius = params.get("sidecut_radius")
+    nose_length = params.get("nose_length")
+    running_length = params.get("running_length")
+    nose_width = params.get("nose_width")
     content = ""
-
-    # 以下为圆弧代码
-    # # 上圆弧
-    # content += ("""<circle cx="{}" cy="{}" r="{}" stroke="black"
-    # stroke-width="1" fill="none"/>""".format(half_overall_length, -cal_waist_width(running_length, sidecut_radius),
-    #                                          sidecut_radius))
-    #
-    # # 下圆弧
-    # content += ("""<circle cx="{}" cy="{}" r="{}" stroke="black"
-    #     stroke-width="1" fill="none"/>""".format(half_overall_length,
-    #                                              cal_waist_width(running_length, sidecut_radius) + nose_width,
-    #                                              sidecut_radius))
 
     step = 1
     code = ""
     code2 = ""
     offset = 0
-    for i, x in enumerate(np.arange(180, 1340.00 + step, step), start=1):
+    for i, x in enumerate(np.arange(nose_length, nose_length + running_length + step, step), start=1):
         y = sqrt(pow(sidecut_radius, 2) - pow(half_overall_length - x, 2))
-        y = 10000 - y
+        y = sidecut_radius - y
         if i == 1:
-            offset = 300 - y
+            offset = nose_width - y
             y = offset + y
             code += "M{} {} ".format(x, y)
-            code2 += "M{} {} ".format(x, 300 - y)
+            code2 += "M{} {} ".format(x, nose_width - y)
         else:
             y += offset
             code += "L{} {} ".format(x, y)
-            code2 += "L{} {} ".format(x, 300 - y)
+            code2 += "L{} {} ".format(x, nose_width - y)
 
-    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code))
-
-    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code2))
+    content += ("""<path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>\
+                <path stroke="#000000" id="svg_3" d="{}" stroke-width="1" fill="none"/>""".format(code, code2))
 
     return content
-
-
-if __name__ == "__main__":
-    pass

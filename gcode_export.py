818ff941dcc7c5a55558402929983b788c5c2e44
zxyle <zxyful@gmail.com>
2019-05-24 15:09:28 +0800

    增加Gcode类

--- gcode_export.py
+++ gcode_export.py
@@ -9,7 +9,42 @@ from datetime import datetime
 
 from config import WEBSITE, COPYRIGHT
 
-filename = "board_profile.nc"
+
+class Gcode:
+    def __init__(self, _filename):
+        self.filename = _filename
+        self.file = self.open_file()
+
+    def open_file(self):
+        return open(self.filename, mode="a", encoding="utf-8")
+
+    def close_file(self):
+        self.file.close()
+
+    def write_code(self, line):
+        self.file.write(line)
+
+    def write_comment(self, line):
+        self.write_code(line)
+
+    def start_gcode(self):
+        self.write_code("G40\n")  # 关闭刀具补偿
+        self.write_code("G49\n")  # 禁用刀具长度补偿
+        self.write_code("G80\n")  # 取消模态动作
+        self.write_code("G54\n")  # 选择坐标系1
+        self.write_code("G90\n")  # 禁用增量移动
+        self.write_code("G21\n")  # 使用毫米长度单位
+        self.write_code("G61\n")  # 确切的路径模式
+        self.write_code("F200.00000\n")  # 设定进给率
+        self.write_code("S1000.00000\n")  # 设置主轴速度
+
+    def end_gcode(self):
+        self.write_code("G0 Z5\n")
+        self.write_code("G0 Z5\n")
+        self.write_code("M2\n")  # 结束程序
+
+
+filename = "board_profile.gcode"
 
 
 def write_line(line):

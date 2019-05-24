50fb89dd20b925096c60ec0ee8add051908ea636
zxyle <zxyful@gmail.com>
2019-05-24 15:08:30 +0800

    增加DPI函数

--- math_tools.py
+++ math_tools.py
@@ -29,18 +29,23 @@ def cal_radius(running_length, camber):
     :return:
     """
     half_running_length = running_length / 2
-    print(half_running_length)
 
     radius = (pow(half_running_length, 2) - pow(camber, 2)) / (2 * camber)
-    # print(radius)
     return radius
 
 
+def mm_to_dpi(mm):
+    """
+    毫米转换成dpi(web上使用的图片都是72dpi 这存在质疑或过时)
+    图像每英寸长度内的像素点数。DPI（Dots Per Inch，每英寸点数）
+    :param mm: 毫米
+    :return: dpi
+    """
+    cm = mm * 0.1
+    inch = cm / 2.54  # 1in = 2.54cm
+    dpi = 72 * inch
+    return dpi
+
+
 if __name__ == "__main__":
-    from svg_export import pack_svg
     r = cal_radius(1260, 12)
-
-    SVG_code = """<circle cx="{}" cy="{}" r="{}" stroke="black" fill="blue" fill-opacity="0.25" />""".format(630,
-                                                                                                             300 + r, r)
-    pack_svg(SVG_code)
-

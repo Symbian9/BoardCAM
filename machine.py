b5037d0380c8680e47dc22e5bb6756c2e0b09a5d
zxyle <zxyful@gmail.com>
2019-06-21 15:12:42 +0800

    增加刀具类

--- machine.py
+++ machine.py
@@ -4,26 +4,53 @@
 # Date: 2019-06-13
 # Desc: 
 
+from math_tools import INCH
+
 
 class CNCRouter:
     def __init__(self, name, unit):
         self.name = name
         self.unit = unit
-        # 文件名
-        # 最后修订日期
-        # 最后修订时间
-        # 软件名称
-        # 程序员
-        # 机床
-        # 控制器
-        # 单位
-        # 加工编号
-        # 操作
-        # 毛坯材料
-        # 材料尺寸
-        # 程序原点
-        # 状态
+        # 加工面积 (工件要小于)
+        # Spindle Speed
+        # Feed rate
+        # 安全加工距离
+        # Drilling speed
+        # 单层步距
 
 
-if __name__ == "__main__":
-    pass
+class RouterBits:
+    def __init__(self, diameter, desc):
+        if diameter[-2:] == "in":
+            self.diameter = self.inch_to_mm(float(eval(diameter[:-2])))
+        elif diameter[-4:] == "inch":
+            self.diameter = self.inch_to_mm(float(eval(diameter[:-4])))
+            print(self.diameter)
+        elif diameter[-2:] == "mm":
+            self.diameter = float(eval(diameter[:-2]))
+        elif diameter[-2:] == "cm":
+            self.diameter = float(eval(diameter[:-2])) * 10
+        else:
+            raise ValueError("diameter variable must specify the unit. (support inch、in、cm、mm)")
+        self.radius = self.cal_radius()
+        self.description = desc
+        self.blade_length = None
+        self.blade_number = 1
+
+    def cal_radius(self):
+        return self.diameter / 2
+
+    @staticmethod
+    def inch_to_mm(inch_value):
+        """
+
+        :param inch_value:
+        :return: mm
+        """
+        return inch_value * INCH
+
+
+if __name__ == '__main__':
+    # cnc = CNCRouter("TigerCNC", "metric")
+    bit = RouterBits("1/4inch", "1/4英寸螺旋向上双刃")
+    print(bit.radius)

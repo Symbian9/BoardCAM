d719d1ac47a49cd6a6d018090aa6b7463521b129
zxyle <zxyful@gmail.com>
2019-06-21 21:53:19 +0800

    完善gcode生成

--- circle.py
+++ circle.py
@@ -15,14 +15,15 @@ def draw_circle_path(cx, cy, r, start_angle=ZERO_ANGLE, end_angle=FULL_ANGLE):
     生成指定圆心的圆路径点
     :param cx: 圆心X坐标
     :param cy: 圆心Y坐标
-    :param r: 半径
+    :param r: 该圆的半径
     :param start_angle:
     :param end_angle:
     :return:
     """
     points = []
     # points_path = ""
-    for angle in range(start_angle, end_angle + 1):
+    angle_step = 2
+    for angle in range(start_angle, end_angle + angle_step, angle_step):
         x = cx + r * cos(angle * pi / STRAIGHT_ANGLE)
         y = cy + r * sin(angle * pi / STRAIGHT_ANGLE)
         points.append(Point(x, y))
@@ -30,3 +31,7 @@ def draw_circle_path(cx, cy, r, start_angle=ZERO_ANGLE, end_angle=FULL_ANGLE):
 
     # print("points_path: {}".format(points_path))
     return points
+
+
+if __name__ == '__main__':
+    print(len(draw_circle_path(10, 10, 5)))
--- gcode_export.py
+++ gcode_export.py
@@ -7,7 +7,8 @@
 
 from datetime import datetime
 
-from config import WEBSITE, COPYRIGHT
+from __version__ import __version__, __title__
+from circle import draw_circle_path
 
 
 class Gcode:
@@ -35,13 +36,20 @@ class Gcode:
         self.write_code("G90\n")  # 禁用增量移动
         self.write_code("G21\n")  # 使用毫米长度单位
         self.write_code("G61\n")  # 确切的路径模式
-        self.write_code("F200.00000\n")  # 设定进给率
+        self.write_code("F2000.00000\n")  # 设定进给率
         self.write_code("S1000.00000\n")  # 设置主轴速度
 
     def end_gcode(self):
         self.write_code("G0 Z5\n")
         self.write_code("G0 Z5\n")
         self.write_code("M2\n")  # 结束程序
+
+    def lift_up(self):
+        """
+        抬起刀具
+        :return:
+        """
+        pass
 
 
 filename = "board_profile.gcode"
@@ -57,35 +65,74 @@ def write_line(line):
         file.write(line)
 
 
-def export_gcode(points):
+def lift_up(self):
+    """
+    抬起刀具
+    :return:
+    """
+    pass
+
+
+def export_gcode(points, insert_coordinate_list):
     """
     生成G-code程序
     :param points: 轮廓路径
+    :param insert_coordinate_list: 嵌件坐标
     :return:
     """
-    write_line("( {} - {} )\n".format(COPYRIGHT, WEBSITE))
-    write_line("( Create Time: {} )\n".format(datetime.now().strftime("%Y-%m-%d %X")))
+    write_line("({})\n".format(48 * "-"))
+    write_line("(文件名··················· {})\n".format(filename))
+    write_line("(最后修订日期··················· {})\n".format(datetime.now().strftime("%Y-%m-%d")))
+    write_line("(最后修订时间··················· {})\n".format(datetime.now().strftime("%X")))
+    write_line("(软件名称··················· {} v{})\n".format(__title__, __version__))
+    write_line("(程序员··················· Zheng)\n")
+    write_line("(机床··················· TigerCNC)\n")
+    write_line("(控制器··················· arduino grbl)\n")
+    write_line("(单位··················· 毫米)\n")
+    write_line("(加工编号··················· 01)\n")
+    write_line("(操作··················· 铣削-钻孔)\n")
+    write_line("(毛坯材料··················· 杨木)\n")
+    write_line("(材料尺寸··················· 165cm*40cm*1cm)\n")
+    write_line("(程序原点··················· X0 -- 左边)\n")
+    write_line("(                           Y0 -- 底边)\n")
+    write_line("(                         Z0 -- 上表面)\n")
+    write_line("(状态··················· 未校验)\n")
+    write_line("(铣刀····················· 6mm螺旋向上双刃)\n")
+    write_line("({})\n".format(48 * "-"))
+
+    write_line("\n")
+
+    # TODO 增加快速移动到加工点
     write_line("G40\n")  # 关闭刀具补偿
     write_line("G49\n")  # 禁用刀具长度补偿
     write_line("G80\n")  # 取消模态动作
     write_line("G54\n")  # 选择坐标系1
-    write_line("G90\n")  # 禁用增量移动
-    write_line("G21\n")  # 使用毫米长度单位
+    write_line("G90\n")  # 禁用增量移动(绝对指令)
+    write_line("G21\n")  # 使用毫米长度单位(公制)
     write_line("G61\n")  # 确切的路径模式
-    write_line("F200.00000\n")  # 设定进给率
+    write_line("F2000.00000\n")  # 设定进给率
     write_line("S1000.00000\n")  # 设置主轴速度
 
     write_line("#2=-1.5\n")
-    write_line("T1 M6\n")
-    write_line("G0 Z5\n")  # 快速移动
-    write_line(" X{} Y{}\n".format(points[0].x, points[0].y))
-    write_line("G1 Z#2\n")
+    write_line("T1 M6\n")  # 刀具
+    # write_line("G0 Z5\n")  # 快速移动
+    write_line("G0 X{} Y{}\n".format(points[0].x, points[0].y))
+    # write_line("G1 Z#2\n")
     path = []
     for point in points:
-        path.append(" X{} Y{}".format(point.x, point.y))
+        path.append("G01 X%.3f Y%.3f Z-1.5" % (point.y, point.x,))
+
+    # inserts_path = []
+
+    for point in sorted(insert_coordinate_list):
+        export_points = draw_circle_path(point.x, point.y, 9)
+        # TODO 每次抬起刀具 深度也得调整 另外一圈深度
+        for p in export_points:
+            path.append("G01 X%.3f Y%.3f" % (p.y, p.x))
+
+        path.append("G01 X%.3f Y%.3f Z 2.0" % (export_points[-1].y, export_points[-1].x))
 
     write_line("\n".join(path))
-
-    write_line("G0 Z5\n")
-    write_line("G0 Z5\n")
+    write_line("\n")
+    write_line("G0 X0 Y0 Z2.0\n".format(points[0].x, points[0].y))
     write_line("M2\n")  # 结束程序

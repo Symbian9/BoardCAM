21b71c3639939f35f2178b30b5ab62ef04a81639
zxyle <zxyful@gmail.com>
2019-06-22 17:52:05 +0800

    modify email info

--- dxf_export.py
+++ dxf_export.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author: Zheng <me@boardcam.org>
+# Author: Zheng <me@BoardCAM.org>
 # Date: 2019-05-26
 # Desc: DXF是AutoCAD(Drawing Interchange Format或者Drawing Exchange Format) 绘图交换文件。
 # http://help.autodesk.com/view/OARX/2018/CHS/?guid=GUID-235B22E0-A567-4CF6-92D3-38A2306D73F3

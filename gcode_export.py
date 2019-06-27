80c51f3a30871e0d75472a5d9c94db992361a88d
zxyle <zxyful@gmail.com>
2019-06-27 09:18:24 +0800

    将Gcode的 finish方法改为私有

--- gcode_export.py
+++ gcode_export.py
@@ -21,7 +21,7 @@ class Gcode:
         self.bit = RouterBits
 
     def close(self):
-        self.finish()
+        self._finish()
         with open(self.filename, mode="w", encoding="utf-8") as file:
             file.write(self.buffer.getvalue())
 
@@ -39,7 +39,7 @@ class Gcode:
         self.write("F1000.00000")  # 设定进给率
         self.write("S1000.00000")  # 设置主轴速度
 
-    def finish(self):
+    def _finish(self):
         self.write("M05 M09")      # 主轴停,冷却液泵马达停
         self.write("G0 X0 Y0 Z5")  # 快速回到坐标原点
         self.write("M2")           # 结束程序  # TODO 比较M2和M30区别
